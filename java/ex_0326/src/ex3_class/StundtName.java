40aab9028bec61439272e6e5cd63992a2e9d629f
euisang0409-prog <euisang0409@gmail.com>
2026-03-26 18:29:39 +0900

    Update StundtName.java

--- java/ex_0326/src/ex3_class/StundtName.java
+++ java/ex_0326/src/ex3_class/StundtName.java
@@ -14,7 +14,27 @@ public class StundtName {
 		
 		Stundt s2 = new Stundt();
 		
+		System.out.println("--------------------------------------");
 		
+		s2.name = "홍길동";
+		s2.age = 260;
+		s2.score = 1000;
+		
+		System.out.println("이름 : " + s2.name);
+		System.out.println("나이 : " + s2.age);
+		System.out.println("점수 : " + s2.score);
+		
+		Stundt s3 = new Stundt();
+		
+		System.out.println("--------------------------------------");
+		
+		s3.name = "고길동";
+		s3.age = 2600;
+		s3.score = 10000;
+		
+		System.out.println("이름 : " + s3.name);
+		System.out.println("나이 : " + s3.age);
+		System.out.println("점수 : " + s3.score);
 		
     }	
 }
