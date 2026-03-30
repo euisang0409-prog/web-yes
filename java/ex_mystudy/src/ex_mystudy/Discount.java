37f5de2e9beb981ab720d2e373ac0140e9dd1d6b
euisang0409-prog <euisang0409@gmail.com>
2026-03-30 18:52:55 +0900

    메시지

--- java/ex_mystudy/src/ex_mystudy/Discount.java
+++ java/ex_mystudy/src/ex_mystudy/Discount.java
@@ -3,7 +3,7 @@ package ex_mystudy;
 public class Discount {
 
 		// 문제
-	
+// final
 //할인률을 경정하는 Discount클래스 
 //할인률을 상수로 가지고 있다.
 //RATE_HIGH = 0.2;
@@ -20,6 +20,23 @@ public class Discount {
 //
 //DiscountMain에서 금액을 키보드에서 입력받아서 할인 적용금액 구하기
 
+	static final double RATE_HIGH = 0.2;
+	static final double RATE_MID = 0.1;
+	static final double RATE_LOW = 0.05;
+	
+	public double disCountRate(int price) {
+		if(price >= 100000) {
+			return RATE_HIGH;
+		}else if(price >= 50000) {
+			return RATE_MID;
+		}else
+			return RATE_LOW;
+	}
+		
+		public double calculatePrice(int price) {
+			double rate = disCountRate(price);
+			return price * (1-rate);
+		}
 	
 	
 	
@@ -27,25 +44,5 @@ public class Discount {
 	
 	
 	
-    // 할인율 상수
-    static final double RATE_HIGH = 0.2;
-    static final double RATE_MID = 0.1;
-    static final double RATE_LOW = 0.05;
-
-    // 금액별 할인율 결정
-    public double discountRate(int price) {
-        if (price >= 100000) {
-            return RATE_HIGH;
-        } else if (price >= 50000) {
-            return RATE_MID;
-        } else {
-            return RATE_LOW;
-        }
-    }
-
-    // 최종 금액 계산
-    public double calculatePrice(int price) {
-        double rate = discountRate(price);
-        return price * (1 - rate);
-    }
+ 
 }
