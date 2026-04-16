ac1232b09fee44c12478acaf55e976a142ba5475
euisang0409-prog <euisang0409@gmail.com>
2026-04-16 10:38:41 +0900

    0416

---
+++ java/ex_0416/src/ex1_bytestream/CoypExample.java
@@ -0,0 +1,5 @@
+package ex1_bytestream;
+
+public class CoypExample {
+
+}
---
+++ java/ex_0416/src/ex1_bytestream/FileOutputStreamExample.java
@@ -0,0 +1,52 @@
+package ex1_bytestream;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamExample {
+	public static void main(String[] args) {
+		FileOutputStream fos = null;
+		try {
+			// 내가 기록하려고 할 때 목적지가 없어질수도 있기 때문에 예외가 발생할 수 있다.
+			// 예외처리를 해줘야 한다.
+			 fos = new FileOutputStream("test.txt");
+			
+			//  목적지까지 가서 작성하려고 할 때 없을수도 있기 때문에
+			// 예외가 발생할 수 있어 예외처리
+			 
+			 // 목적지에 파일이나 폴더가 없으면 만들어준다
+			fos.write('f');
+			fos.write('i');
+			fos.write('l');
+			fos.write('e');
+			
+			
+			String msg = "fileOutput 예제입니다. \n";
+			String msg2 = "여러줄도 가능합니다.\n";
+			
+			// 문자열을 byte배열로 변환하기
+			fos.write(msg.getBytes());
+			fos.write(msg2.getBytes());
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch(IOException e) {
+			
+			e.printStackTrace();
+		}finally {
+			try {
+				if(fos != null) {
+					fos.close();
+				}			
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+}
