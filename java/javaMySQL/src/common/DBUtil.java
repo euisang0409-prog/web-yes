87eaf79fcca34921d2667134d6df923058433572
euisang0409-prog <euisang0409@gmail.com>
2026-05-13 18:22:08 +0900

    0513

---
+++ java/javaMySQL/src/common/DBUtil.java
@@ -0,0 +1,31 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+
+    private static String url =
+        "jdbc:mysql://localhost:3306/java_db";
+
+    private static String user = "root";
+
+    private static String password = "11111111";
+
+    public static Connection getConnection() {
+
+        try {
+
+            Connection conn =
+                DriverManager.getConnection(url, user, password);
+
+            return conn;
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
