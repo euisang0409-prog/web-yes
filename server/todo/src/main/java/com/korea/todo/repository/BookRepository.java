ec4a27d74e56ff68db814aa1929aa188f2607bd5
euisang0409-prog <euisang0409@gmail.com>
2026-07-21 17:56:57 +0900

    0721

--- server/todo/src/main/java/com/korea/todo/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.korea.todo.repository;
-
-
-//JapRepository<BookEntitiy, Long> 상속받으면 Repository로 취급한다.
-public interface BookRepository extends JapRepository<BookEntitiy, Long>{
-
-	private final BookRepository bookRepository;
-	
-}
--- server/todo/src/main/java/com/korea/todo/repository/UserRepository.java
+++ server/todo/src/main/java/com/korea/todo/repository/UserRepository.java
@@ -4,13 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.korea.todo.entity.UserEntity;
 
-public interface UserRepository extends JpaRepository<UserEntity, String>{
-	
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+	//전달한 아이디를 가지는 데이터를 조회
 	UserEntity findByUsername(String username);
 	
+	//전달한 아이디를 가지는 데이터가 있는지 검사
 	Boolean existsByUsername(String username);
-
-	UserEntity	findByUsernameAndPassword(String username, String password);
-
 	
+	//전달한 아이디와 비밀번호를 가지는 데이터를 조회
+ 	UserEntity findByUsernameAndPassword(String username, String password);
 }
